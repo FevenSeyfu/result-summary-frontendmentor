687f75990869a0917331aa21db65c46f4444bd88
FevenSeyfu <fevenseyfu00@gmail.com>
2024-03-08 19:06:59 +0300

    refactor the result to be fetched and calculate the avarage from data.json

--- script.js
+++ script.js
@@ -2,10 +2,26 @@
 fetch("./data.json")
   .then((response) => response.json())
   .then((data) => {
+    const header = document.querySelector('header');
+
     const summaryContainer = document.querySelector(".summary-container");
     const button = document.querySelector(".summary-container button");
 
+    // get the average score
+    const sum = data.reduce((total, item) => total + item.score, 0);
+    const averageScore = Math.floor(sum / data.length);
+
+    const resultCircle = document.createElement("div");
+    resultCircle.className = "result-circle";
+    resultCircle.innerHTML = `<span>${averageScore}</span> of 100`;
+
+    // Insert resultCircle after header
+    header.parentNode.insertBefore(resultCircle, header.nextSibling);
+
     data.forEach((item) => {
+      // results section
+      const sum = data.reduce((total, item) => total + item.score, 0);
+      const averageScore = Math.floor(sum / data.length);
       const categoryDiv = document.createElement("div");
       categoryDiv.className = `summary-box ${item.category.toLowerCase()}`;
       categoryDiv.innerHTML = `
